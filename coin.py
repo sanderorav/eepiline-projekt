c6987b9a58224ab755e6528c4ec7bbafbb93923c
Suur Toss <sander.orav@voco.ee>
2024-04-16 14:42:41 +0300

    Added coins

---
+++ coin.py
@@ -0,0 +1,25 @@
+import pygame
+import random
+
+class Coin(pygame.sprite.Sprite):
+    
+    def __init__(self, screen, game_settings, stats):
+        super(Coin, self).__init__()
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.coin = pygame.image.load('coin.png').convert_alpha()
+        self.rect = self.coin.get_rect(
+            center=(
+                random.randint(game_settings.screen_height + 20, game_settings.screen_width + 100),
+                random.randint(0, game_settings.screen_height),
+                )
+            )
+        self.speed = random.randint(stats.min_speed, stats.max_speed)
+        
+    def blit_me(self):
+        self.screen.blit(self.coin, self.rect)
+        
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
--- functions.py
+++ functions.py
@@ -1,15 +1,13 @@
 import sys
 import pygame
 from player import Player
+from coin import Coin
 
 pygame.init()
+ADDCOIN = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDCOIN, 500)
 
-def update_screen(game_settings, screen, player):
-    screen.fill(game_settings.bg_colour)
-    player.blit_me()
-    pygame.display.flip()
-    
-def check_events(game_settings, screen, player):
+def check_events(game_settings, screen, player, coins, stats):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -31,3 +29,23 @@ def check_events(game_settings, screen, player):
                 player.moving_up = False
             if event.key == pygame.K_DOWN:
                 player.moving_down = False
+        elif event.type == ADDCOIN:
+            add_coin(game_settings, screen, coins, stats)
+
+def update_screen(game_settings, screen, player, coins, clock):
+    screen.fill(game_settings.bg_colour)
+    player.blit_me()
+    if len(coins) > 0:
+        for coin in coins:
+            coin.blit_me()
+    clock.tick(120)
+    pygame.display.flip()
+    
+def add_coin(game_settings, screen, coins, stats):
+    new_coin = Coin(screen, game_settings, stats)
+    coins.add(new_coin)
+
+def update_coins(player, coins, stats, game_settings):
+    hitted_coin = pygame.sprite.spritecollideany(player, coins)
+    if hitted_coin != None:
+        hitted_coin.kill()
