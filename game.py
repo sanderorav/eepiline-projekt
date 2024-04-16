b297ffa1a92b46c2de842a3c94ca7bbebe090c9a
sanderorav <150243675+sanderorav@users.noreply.github.com>
2024-04-16 20:07:31 +0300

    Add scoreboard (#2)

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from player import Player
 from coin import Coin
 import functions as func
@@ -16,15 +17,17 @@ def run_game():
     
     stats = GameStats()
     
+    sb = Scoreboard(gm_settings, screen, stats)
+    
     player = Player(screen)
     
     coins = pygame.sprite.Group()
     
     while True:
         func.check_events(gm_settings, screen, player, coins, stats)
-        func.update_screen(gm_settings, screen, player, coins, clock)
+        func.update_screen(gm_settings, screen, player, coins, clock, sb)
         player.update()
-        func.update_coins(player, coins, stats, gm_settings)
+        func.update_coins(player, coins, stats, sb, gm_settings)
         coins.update()
     
 run_game()
