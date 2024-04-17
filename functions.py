297708665564f1d28a477b18df7427247632c066
Suur Toss <sander.orav@voco.ee>
2024-04-17 17:54:32 +0300

    Robbers appear on screen [Delivers #187442669]

--- functions.py
+++ functions.py
@@ -2,12 +2,15 @@ import sys
 import pygame
 from player import Player
 from coin import Coin
+from robber import Robber
 
 pygame.init()
 ADDCOIN = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDCOIN, 500)
+ADDROBBER = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDROBBER, 1500)
 
-def check_events(game_settings, screen, player, coins, stats):
+def check_events(game_settings, screen, player, coins, robbers, stats):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -31,13 +34,18 @@ def check_events(game_settings, screen, player, coins, stats):
                 player.moving_down = False
         elif event.type == ADDCOIN:
             add_coin(game_settings, screen, coins, stats)
+        elif event.type == ADDROBBER:
+            add_robber(game_settings, screen, robbers, stats)
 
-def update_screen(game_settings, screen, player, coins, clock, sb):
+def update_screen(game_settings, screen, player, coins, robbers, clock, sb):
     screen.fill(game_settings.bg_colour)
     player.blit_me()
     if len(coins) > 0:
         for coin in coins:
             coin.blit_me()
+    if len(robbers) > 0:
+        for robber in robbers:
+            robber.blit_me()
     sb.draw_score()
     clock.tick(120)
     pygame.display.flip()
@@ -45,6 +53,10 @@ def update_screen(game_settings, screen, player, coins, clock, sb):
 def add_coin(game_settings, screen, coins, stats):
     new_coin = Coin(screen, game_settings, stats)
     coins.add(new_coin)
+    
+def add_robber(game_settings, screen, robbers, stats):
+    new_robber = Robber(screen, game_settings, stats)
+    robbers.add(new_robber)
 
 def update_coins(player, coins, stats, sb, game_settings):
     hitted_coin = pygame.sprite.spritecollideany(player, coins)
@@ -52,3 +64,12 @@ def update_coins(player, coins, stats, sb, game_settings):
     if hitted_coin != None:
         stats.score += 1
         hitted_coin.kill()
+
+def update_robbers(player, robbers, stats, sb, game_settings):
+    hitted_robber = pygame.sprite.spritecollideany(player, robbers)
+    if hitted_robber != None and not player.is_penalised:
+        stats.score -= 5
+        player.is_penalised = True
+    elif hitted_robber == None:
+        player.is_penalised = False
+    sb.prepare_score()
