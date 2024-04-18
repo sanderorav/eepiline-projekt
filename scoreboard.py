c35e942837b281e136a15697ef598696c6bd5e07
Suur Toss <sander.orav@voco.ee>
2024-04-18 18:36:54 +0300

    Added levels

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard():
         self.text_color = (0, 0, 0)
         self.font = pygame.font.SysFont(None, 46)
         self.prepare_score()
+        self.prepare_level()
         
     def prepare_score(self):
         score_str = str(self.stats.score)
@@ -21,6 +22,14 @@ class Scoreboard():
         self.score_coin_rect.left = self.screen_rect.left + 10
         self.score_coin_rect.top = 10
         
+    def prepare_level(self):
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render('LEVEL: ' + level_str, True, self.text_color, self.game_settings.bg_colour)
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.left = self.screen_rect.left + 10
+        self.level_rect.top = 60
+        
     def draw_score(self):
         self.screen.blit(self.score_image, self.score_image_rect)
         self.screen.blit(self.score_coin, self.score_coin_rect)
+        self.screen.blit(self.level_image, self.level_rect)
