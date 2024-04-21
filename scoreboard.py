c14453d170d3cd60ebf8b14292c6abd4669b1a91
Suur Toss <sander.orav@voco.ee>
2024-04-21 16:28:25 +0300

    Added records

--- scoreboard.py
+++ scoreboard.py
@@ -10,6 +10,7 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 46)
         self.prepare_score()
         self.prepare_level()
+        self.prepare_record()
         
     def prepare_score(self):
         score_str = str(self.stats.score)
@@ -28,8 +29,16 @@ class Scoreboard():
         self.level_rect = self.level_image.get_rect()
         self.level_rect.left = self.screen_rect.left + 10
         self.level_rect.top = 60
+    
+    def prepare_record(self):
+        record_str = str(self.stats.record)
+        self.record_image = self.font.render('HI-SCORE: ' + record_str, True, self.text_color, self.game_settings.bg_colour)
+        self.record_rect = self.record_image.get_rect()
+        self.record_rect.right = self.screen_rect.right - 10
+        self.record_rect.top = 20
         
     def draw_score(self):
         self.screen.blit(self.score_image, self.score_image_rect)
         self.screen.blit(self.score_coin, self.score_coin_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.screen.blit(self.record_image, self.record_rect)
