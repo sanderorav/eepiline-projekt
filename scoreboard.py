3a85e47a26f93d1020c44f044042a80f726dfd56
Suur Toss <sander.orav@voco.ee>
2024-04-23 19:28:58 +0300

    Adding everything to the main branch

--- scoreboard.py
+++ scoreboard.py
@@ -10,11 +10,15 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 46)
         self.prepare_score()
         self.prepare_level()
+        self.prepare_record()
         
     def prepare_score(self):
         score_str = str(self.stats.score)
-        self.score_image = self.font.render(score_str, True, self.text_color, self.game_settings.bg_colour)
-        self.score_coin = pygame.image.load('coin.png').convert_alpha()
+        temp_surface = self.font.render(score_str, True, self.text_color)
+        self.score_image = pygame.Surface(temp_surface.get_size(), pygame.SRCALPHA)
+        self.score_image.fill((255, 255, 255, 0))
+        self.score_image.blit(temp_surface, (0, 0))
+        self.score_coin = pygame.image.load('coin_parem_kui_varem.png').convert_alpha()
         self.score_image_rect = self.score_image.get_rect()
         self.score_image_rect.left = self.screen_rect.left + 70
         self.score_image_rect.top = 20
@@ -24,12 +28,26 @@ class Scoreboard():
         
     def prepare_level(self):
         level_str = str(self.stats.level)
-        self.level_image = self.font.render('LEVEL: ' + level_str, True, self.text_color, self.game_settings.bg_colour)
+        temp_surface = self.font.render('LEVEL: ' + level_str, True, self.text_color)
+        self.level_image = pygame.Surface(temp_surface.get_size(), pygame.SRCALPHA)
+        self.level_image.fill((255, 255, 255, 0))
+        self.level_image.blit(temp_surface, (0, 0)) 
         self.level_rect = self.level_image.get_rect()
         self.level_rect.left = self.screen_rect.left + 10
         self.level_rect.top = 60
+    
+    def prepare_record(self):
+        record_str = str(self.stats.record)
+        temp_surface = self.font.render('HI-SCORE: ' + record_str, True, self.text_color)
+        self.record_image = pygame.Surface(temp_surface.get_size(), pygame.SRCALPHA)
+        self.record_image.fill((255, 255, 255, 0))
+        self.record_image.blit(temp_surface, (0, 0))
+        self.record_rect = self.record_image.get_rect()
+        self.record_rect.right = self.screen_rect.right - 10
+        self.record_rect.top = 20
         
     def draw_score(self):
         self.screen.blit(self.score_image, self.score_image_rect)
         self.screen.blit(self.score_coin, self.score_coin_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.screen.blit(self.record_image, self.record_rect)
